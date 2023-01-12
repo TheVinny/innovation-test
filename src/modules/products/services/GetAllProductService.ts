e13b0a141ce63970dd11068d6cb770bf3b210967
vinny <vinny.vip07@gmail.com>
2023-01-12 19:47:22 -0300

    feat: create Get all product service

---
+++ src/modules/products/services/GetAllProductService.ts
@@ -0,0 +1,18 @@
+import { inject, injectable } from 'tsyringe';
+import { IProduct } from '../domain/interfaces/IProduct';
+import { IProductRepository } from '../domain/interfaces/IProductRepository';
+
+@injectable()
+class GetAllProductService {
+  constructor(
+    @inject('ProductRepository')
+    private productRepository: IProductRepository,
+  ) {}
+  async execute(): Promise<IProduct[]> {
+    const products = await this.productRepository.GetAll();
+
+    return products;
+  }
+}
+
+export default GetAllProductService;
