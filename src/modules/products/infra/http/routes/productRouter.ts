f3ce24301356977a27ee7b693165021fe62a6b5c
vinny <vinny.vip07@gmail.com>
2023-01-12 19:48:59 -0300

    feat: create product router

--- src/modules/products/infra/http/routes/productRouter.ts
+++ src/modules/products/infra/http/routes/productRouter.ts
@@ -1,7 +1,53 @@
-import { Router } from "express";
+import { Router } from 'express';
+import CreateProductControler from '../controllers/CreateProductControler';
+import DeleteByIdController from '../controllers/DeleteByIdController';
+import GetAllProductsController from '../controllers/GetAllProductsController';
+import GetByIdProductController from '../controllers/GetByIdProductController';
+import UpdateProductController from '../controllers/UpdateProductController';
+import { Joi, celebrate, Segments } from 'celebrate';
 
 const productRouter: Router = Router();
 
-productRouter.get("/");
+//MIddlewares de validação das rotas
+
+const middlewares = {
+  create: celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      quantity: Joi.number().required(),
+      status: Joi.string().required().valid('Active', 'Inactive'),
+      category: Joi.string().required(),
+    },
+  }),
+  update: celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+
+    [Segments.BODY]: {
+      name: Joi.string(),
+      quantity: Joi.number(),
+      status: Joi.string().valid('Active', 'Inactive'),
+      category: Joi.string(),
+    },
+  }),
+
+  validID: celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required().uuid(),
+    },
+  }),
+};
+
+productRouter.post('/', middlewares.create, CreateProductControler.execute);
+
+productRouter.get('/', GetAllProductsController.execute);
+productRouter.get(
+  '/:id',
+  middlewares.validID,
+  GetByIdProductController.execute,
+);
+productRouter.delete('/:id', middlewares.validID, DeleteByIdController.execute);
+productRouter.put('/:id', middlewares.update, UpdateProductController.execute);
 
 export default productRouter;
