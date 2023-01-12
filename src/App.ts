8b178053e11d11b3ca8fdc8438875c735892c80e
vinny <vinny.vip07@gmail.com>
2023-01-12 19:40:16 -0300

    feat: adding reflect metadata and tsynringe container

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,7 @@
-import Server from "@shared/infra/http/Server";
-import "dotenv/config";
+import 'reflect-metadata';
+import 'dotenv/config';
+import '@shared/infra/database';
+import '@shared/container';
+import Server from '@shared/infra/http/Server';
 
 new Server().start(process.env.BACKEND_PORT || 2222);
