2168068e6dc0d230a3f830b59a74337e896df1cf
vinny <vinny.vip07@gmail.com>
2023-01-12 19:51:28 -0300

    feat: create poduct interface

---
+++ src/modules/products/domain/interfaces/IProduct.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  id: string;
+  name: string;
+  category: string;
+  status: string;
+  quantity: number;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date;
+}
